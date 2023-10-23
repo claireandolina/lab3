98de4f135bc28af0942b539f8357ff055ec4e202
Claire <claire.andolina@gmail.com>
2023-10-23 11:33:00 -0700

    Fixed Bugs in reverseInPlace and reversed methods

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -3,36 +3,44 @@
 public class ArrayExamples {
 
   // Changes the input array to be in reversed order
-  static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+  static int[] reverseInPlace(int[] arr) { //1,2,3,4,5
+    for(int i = 0; i < arr.length/2; i += 1) { //only need to do for half the values to swap all
+      int temp=arr[i]; //temp=1, 2
+      arr[i]=arr[arr.length-i-1]; //arr[0] = 5, arr[1]=4
+      arr[arr.length-i-1]=temp; //arr[4] = 1   //need a placeholder temp var and swap in-place or else the values will keep
+                                              //overriding itself as it iterates through. after halfway through,
+                                              //the array will have repeating values
     }
+    return arr; //also needs return array
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+    for(int i = 0; i < arr.length; i += 1) 
+    { //
+      newArray[i] = arr[arr.length-i-1]; //this should be say newArray[arr.length-i-1] = arr[i]. setting
+                                       // old array equal to new array values now (which is empty)
+      //new array's first index = arr's last index
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
   static double averageWithoutLowest(double[] arr) {
-    if(arr.length < 2) { return 0.0; }
+    if(arr.length < 2) { return 0.0; } // shouldnt we return just the one element in the array?
     double lowest = arr[0];
     for(double num: arr) {
-      if(num < lowest) { lowest = num; }
+      if(num < lowest) { lowest = num; } //find lowest element in array
     }
     double sum = 0;
     for(double num: arr) {
       if(num != lowest) { sum += num; }
     }
-    return sum / (arr.length - 1);
+    return sum / (arr.length - 1); // BUG: must divide by arr.length - 2 because not including lowest element
   }
 
 
--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,36 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    int[] input2 = {1,2,3,4,5};
+    int[] input3 = {0,0};
+    int[] input4 = {-2,1,7};
+    int[] input5 = { };
+    assertArrayEquals(new int[]{3}, ArrayExamples.reverseInPlace(input1));
+    assertArrayEquals(new int[]{5,4,3,2,1}, ArrayExamples.reverseInPlace(input2));
+    assertArrayEquals(new int[]{0,0}, ArrayExamples.reverseInPlace(input3));
+    assertArrayEquals(new int[]{7,1,-2}, ArrayExamples.reverseInPlace(input4));
+    assertArrayEquals(new int[]{ }, ArrayExamples.reverseInPlace(input5));
 	}
-
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = {1,2,3,4,5};
+    int[] input3 = {0,0};
+    int[] input4 = {-2,1,7};
+    int[] input5 = {1};
+    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{5,4,3,2,1}, ArrayExamples.reversed(input2));
+    assertArrayEquals(new int[]{0,0}, ArrayExamples.reversed(input3));
+    assertArrayEquals(new int[]{7,1,-2}, ArrayExamples.reversed(input4));
+    assertArrayEquals(new int[]{1}, ArrayExamples.reversed(input5));
+  }
+
+/* 
+  @Test
+  public void averageWithoutLowest() {
+    int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+*/
 }
